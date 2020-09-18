2216dc02d32c0d09481f6e6085ace6fdd523b7b1
whp98 <whp98@outlook.com>
2020-09-18 14:59:31 +0800

    测试了一些java基础内容

--- src/test/java/basic/factorypatten/ShapeFactoryTest.java
+++ src/test/java/basic/factorypatten/ShapeFactoryTest.java
@@ -1,11 +1,16 @@
 package basic.factorypatten;
-
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.*;
 public class ShapeFactoryTest{
     @Test
     public void shapeTest(){
+        long times = System.nanoTime();
+        long timemimi = System.currentTimeMillis();
         ShapeFactory shapeFactory = new ShapeFactory();
         //获取Circle对象并调用draw方法
         Shape shape1 = shapeFactory.getShape("CIRCLE");
@@ -19,5 +24,38 @@ public class ShapeFactoryTest{
         Shape shape3 = shapeFactory.getShape("SQUARE");
         assertNotNull(shape3);
         shape3.draw();
+        System.out.println(System.nanoTime()-times);
+        System.out.println(System.currentTimeMillis()-timemimi);
+        System.out.println(LocalDate.now().lengthOfYear());
+        System.out.println(LocalDate.now().lengthOfMonth());
+    }
+    //PAC: 遍历删除使用iterator来进行
+    @Test
+    public void foreachTest(){
+        List<String> list = new ArrayList<>();
+        list.add("2");
+        list.add("2");
+        Iterator<String> iterator = list.listIterator();
+        while(iterator.hasNext()){
+            String item = (String) iterator.next();
+            if("2".equals(item)){
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
+    }
+    //P3C注意map是否允许存入null值
+    @Test
+    public void mapSaveNullTest(){
+        TreeMap<String, String> treeMap = new TreeMap<>();
+        treeMap.put("hello",null);
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("sdhajd",null);
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        //报错
+        //concurrentHashMap.put("sdhakjdh", null);
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        //NPE
+        //hashtable.put("dshakjdjas",null);
     }
 }
